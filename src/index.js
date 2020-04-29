6a4c040e10bb95b5d075402e0b0217f3841d2b67
Mohamed Naser <nc_m.naser@hotmail.com>
2020-04-30 01:34:06 +0200

    fix eslint issues

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const homeTabButton = document.getElementById("home-tab");
-const menuTabButton = document.getElementById("menu-tab");
-const homeDiv = document.getElementById("home-container");
-const menuDiv = document.getElementById("menu");
-
-homeTabButton.addEventListener("click", clickHomeTabButton);
-menuTabButton.addEventListener("click", clickMenuTabButton);
+const homeTabButton = document.getElementById('home-tab');
+const menuTabButton = document.getElementById('menu-tab');
+const homeDiv = document.getElementById('home-container');
+const menuDiv = document.getElementById('menu');
 
 function clickHomeTabButton() {
-  menuDiv.style.display = "none";
-  homeDiv.style.display = "block";
+  menuDiv.style.display = 'none';
+  homeDiv.style.display = 'block';
 }
 
 function clickMenuTabButton() {
-  homeDiv.style.display = "none";
-  menuDiv.style.display = "block";
+  homeDiv.style.display = 'none';
+  menuDiv.style.display = 'block';
 }
+
+homeTabButton.addEventListener('click', clickHomeTabButton);
+menuTabButton.addEventListener('click', clickMenuTabButton);
